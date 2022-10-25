cf17c4eb0c953bd7bed9cfbc00d1b52dcfe912d3
aravindsabu <aravindsabu2001@gmail.com>
2022-10-25 12:32:13 +0530

    ok

---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  viewbooks=()=>{
+    return this.http.get("http://localhost:8080/viewbooks")
+  }
+  addbooks=(data:any)=>{
+    return this.http.post("http://localhost:8080/bookadd",data)
+  }
+  deletebooks=(data:any)=>{
+    return this.http.post("http://localhost:8080/deleteBook",data)
+  }
+}
